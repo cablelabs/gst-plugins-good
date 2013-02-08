4710099d9613033be77d69fdf7b296215274f831
Thiago Santos <thiago.sousa.santos@collabora.com>
2013-02-07 21:49:42 -0300

    qtdemux: fixing setcaps to be called repeatedly

--- gst/isomp4/qtdemux.c
+++ gst/isomp4/qtdemux.c
@@ -1662,6 +1662,11 @@ gst_qtdemux_setcaps (GstQTDemux * demux, GstCaps * caps)
     demux->fragmented = TRUE;
     demux->mss_mode = TRUE;
 
+    if (demux->n_streams > 1) {
+      /* can't do this, we can only renegotiate for another mss format */
+      return FALSE;
+    }
+
     value = gst_structure_get_value (structure, "media-caps");
     /* create stream */
     if (value) {
@@ -1669,9 +1674,13 @@ gst_qtdemux_setcaps (GstQTDemux * demux, GstCaps * caps)
 
       /* TODO update when stream changes during playback */
 
-      stream = _create_stream ();
-      demux->streams[demux->n_streams] = stream;
-      demux->n_streams = 1;
+      if (demux->n_streams == 0) {
+        stream = _create_stream ();
+        demux->streams[demux->n_streams] = stream;
+        demux->n_streams = 1;
+      } else {
+        stream = demux->streams[0];
+      }
 
       timescale_v = gst_structure_get_value (structure, "timescale");
       if (timescale_v) {
@@ -1683,6 +1692,10 @@ gst_qtdemux_setcaps (GstQTDemux * demux, GstCaps * caps)
       demux->timescale = stream->timescale;
 
       mediacaps = gst_value_get_caps (value);
+      if (!stream->caps || !gst_caps_is_equal_fixed (mediacaps, stream->caps)) {
+        GST_DEBUG_OBJECT (demux, "We have a new caps %" GST_PTR_FORMAT,
+            mediacaps);
+      }
       gst_caps_replace (&stream->caps, (GstCaps *) mediacaps);
       structure = gst_caps_get_structure (mediacaps, 0);
       if (g_str_has_prefix (gst_structure_get_name (structure), "video")) {
